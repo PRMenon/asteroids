130611c5d58273eecdfdb411316749978e90971f
PRMenon <pratheek.menon@gmail.com>
2025-02-09 03:24:10 +0000

    added player controls

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
@@ -13,6 +16,8 @@ def main():
                 return
         
         pygame.Surface.fill(screen, (0,0,0))
+        p.update(dt)
+        p.draw(screen)
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
